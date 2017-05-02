a8366768302c07e3f136e4cf9eee34468c83ce45
TerrenceBeckham <terrencebeckham@icloud.com>
2017-05-02 08:56:35 +0200

    Testing

--- db/migrate/20170501101809_create_courses.rb
+++ db/migrate/20170501101809_create_courses.rb
@@ -1,6 +1,6 @@
 class CreateCourses < ActiveRecord::Migration[5.0]
   def change
-    create_table :courses do |t| #t stands for table I thinkrake
+    create_table :courses do |t|
       t.string :title
       t.text :description
       t.decimal :cost
