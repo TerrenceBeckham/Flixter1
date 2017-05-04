7d9c5b331566e0193c8fcf208d8143207474ee18
TerrenceBeckham <terrencebeckham@icloud.com>
2017-05-04 08:57:34 +0200

    Set page that lists out all of the courses

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -8,7 +8,7 @@ class Instructor::CoursesController < ApplicationController
   def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
-      redirect_to instructor_courses_path(@course)
+      redirect_to instructor_course_path(@course)
     else
       render :new, status: :unprocessable_entity
     end
@@ -16,7 +16,7 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @course =Course.find(params[:id])
+    @course = Course.find(params[:id])
   end
 
   private
